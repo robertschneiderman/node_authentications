495f6f890721a68f73892a9be0d79befebb5831e
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-08 22:37:32 -0800

    added salting and hashing of password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,38 @@
-const Authetication = require('./controllers/authentication');
+const User = require('../models/user');
 
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    res.status(422).send({ error: 'You must provide email and password'});
+  }
+
+  // See if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    
+    if (err) { return next(err); }
+
+    if (existingUser) {
+      return res.status(422).send({error: 'Email is in use' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      res.json(user);
+    })
+
+  });
+  // If a user with email does exist, return an error
+
+
+  // Respond to request indicating that the user was created
+
 }
