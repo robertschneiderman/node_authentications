b52c889dfecb7dd99afd977519485fe31c05312f
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-08 20:37:47 -0800

    user schema

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define our model
+
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+})
+
+// Create the model class
+
+const modelClass = mongoose.model('user', userSchema);
+
+// Export the model
+module.exports = ModelClass;
